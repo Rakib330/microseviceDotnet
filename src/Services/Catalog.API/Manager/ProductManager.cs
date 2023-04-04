d93aba6876d65efce9be8e10309a6035523c2969
MD Rakibul Hasan <59144192+Rakib330@users.noreply.github.com>
2023-04-04 21:55:36 +0600

    seed mathod

--- src/Services/Catalog.API/Manager/ProductManager.cs
+++ src/Services/Catalog.API/Manager/ProductManager.cs
@@ -1,6 +1,14 @@
-﻿namespace Catalog.API.Manager
+﻿using Catalog.API.Models;
+using Catalog.API.Repository;
+using MongoRepo.Manager;
+using MongoRepo.Repository;
+
+namespace Catalog.API.Manager
 {
-    public class ProductManager
+    public class ProductManager : CommonManager<Product>
     {
+        public ProductManager() : base(new ProductRepository())
+        {
+        }
     }
 }
