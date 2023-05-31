f7d300c86a4da4d9fea35243c933420fd949ad79
MD Rakibul Hasan <59144192+Rakib330@users.noreply.github.com>
2023-05-31 23:47:59 +0600

    proto

---
+++ src/Services/Discount.Grpc/Repository/ICouponRepository.cs
@@ -0,0 +1,12 @@
+﻿using Discount.Grpc.Modals;
+
+namespace Discount.Grpc.Repository
+{
+    public interface ICouponRepository
+    {
+        Task<Coupon> GetDiscount(string productId);
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productId);
+    }
+}
