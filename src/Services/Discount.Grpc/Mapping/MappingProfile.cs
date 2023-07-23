503f4180800f30010d528eb66b62fdc2f6d53de7
MD Rakibul Hasan <59144192+Rakib330@users.noreply.github.com>
2023-07-23 08:07:52 +0600

    asd

---
+++ src/Services/Discount.Grpc/Mapping/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Discount.Grpc.Modals;
+using Discount.Grpc.Protos;
+
+namespace Discount.Grpc.MappingProfile
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile() { 
+        CreateMap<Coupon,CouponRequest>().ReverseMap();
+        }
+    }
+}
---
+++ src/Services/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Discount.Grpc.Protos;
+using Discount.Grpc.Repository;
+using Grpc.Core;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService:DiscountProtoService.DiscountProtoServiceBase
+    {
+        CouponRepository _couponRepository;
+        Logger<DiscountService> _logger;
+        IMapper _mapper;
+        public DiscountService(CouponRepository couponRepository, Logger<DiscountService> logger,IMapper mapper)
+        {
+            _couponRepository = couponRepository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        public override async Task<CouponRequest> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await _couponRepository.GetDiscount(request.ProductId);
+
+            if(coupon == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, "Discount Not Found"));
+            }
+            _logger.LogInformation("Discount is retrived for ProductName : {productName}, Amount :{Amount}", coupon.ProductName, coupon.Amount);
+            //return new CouponRequest { ProductId = coupon.ProductId,ProductName = coupon.ProductName,Description = coupon.Description,Amount=coupon.Amount};
+            return _mapper.Map<CouponRequest>(coupon);
+        }
+    }
+}
